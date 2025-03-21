c5ccd88a311b7da0d8a0664b4ef8ef2cf8dad974
Kiselev Vladimir <47751639+ches47@users.noreply.github.com>
2025-03-21 22:03:20 +0200

    add types, product

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
+import ProductList from '@/components/Shared/Product/product-list';
+import sampleData from '@/db/sample-data';
+
 export const metadata = {
   title: 'Main Page',
 };
 
 const HomePage = () => {
-  return <div>Main page</div>;
+  return <ProductList data={sampleData.products} title="New products" limit={4} />;
 };
 
 export default HomePage;
