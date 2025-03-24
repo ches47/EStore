9074ada207e087af8c1f69687691f93822623287
Kiselev Vladimir <47751639+ches47@users.noreply.github.com>
2025-03-24 17:52:28 +0200

    add Product[slug]
    add Product[slug]

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,10 +1,9 @@
 'use server';
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '@/db/prisma';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
 import { convertToPlainObject } from '../utils';
 
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: {
@@ -12,4 +11,12 @@ export async function getLatestProducts() {
     },
   });
   return convertToPlainObject(data);
+}
+
+export async function getSingleProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: {
+      slug: slug,
+    },
+  });
 }
